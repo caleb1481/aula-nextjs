4cb58bc08da4d70be23cc03ebf58ea2f54fb879c
BRUNO CALEB DOS SANTOS <bruno.santos1481>
2026-03-20 11:01:49 -0300

    aa

---
+++ src/app/atividades/atividade01.jsx
@@ -0,0 +1,8 @@
+import styles from "./at01.css";
+function atv01(){
+    return(
+        <div>
+            <h1>sla man</h1>
+        </div>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,7 @@ function Home() {
       <p>Nesta aula iremos entender o uso de CSS global e local.</p>
       <label>Exemplo Label</label>
       <small>Exemplo Small</small>
+      <link href="./atividades/atividade01.jsx">sla</link>
       <div className={styles.conteinerExPosicao}>
         <div className={styles.divVermelha}></div>
         <div className={styles.divVerde}></div>
