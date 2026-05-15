4ac14e7136c3b375aee36487005ccf0a691c6675
Brunocaleb-dev <brunocaleb77@gmail.com>
2026-05-15 11:44:38 -0300

    aula

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import styles from "./page.module.css";
 function Home() {
   return (
     <div className={styles.containerHome} > 
-      <div className={styles.containerListas}>
+      <div className={styles.containerExemplo}>
         <h1>Exemplos</h1>
         <Link href="/exemplos/ex-01">Exemplo 1</Link>
         <Link href="/exemplos/ex-02">Exemplo 2</Link>        
@@ -13,17 +13,14 @@ function Home() {
         <Link href="/exemplos/ex-04">Exemplo 4</Link>     
       </div>
 
-      <div className={styles.containerListas}>
+      <div className={styles.containerExPosicao}>
         <h1>Atividades</h1>
         <Link href="/atividades/atv-01">Atividade 1</Link>
         <Link href="/atividades/atv-02">Atividade 2</Link>
         <Link href="/atividades/atv-03">Atividade 3</Link>
         <Link href="/atividades/atv-04">Atividade 4</Link>
-<<<<<<< HEAD
-=======
         <Link href="/atividades/atv-05">Atividade 5</Link>
 
->>>>>>> bbf20e35c82ebaa6085a68b84cba2d67c00b6d13
       </div>
 
     </div>
